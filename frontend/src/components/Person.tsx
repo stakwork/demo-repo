6fd14c93d160ffb631a8c80381b76181059ebf32
Evanfeenstra <evanfeenstra@gmail.com>
2025-05-15 16:14:12 -0700

    rm

--- frontend/src/components/Person.tsx
+++ frontend/src/components/Person.tsx
@@ -3,18 +3,3 @@ export interface Person {
   name: string;
   email: string;
 }
-export class People {
-  people: Person[];
-
-  constructor() {
-    this.people = [];
-  }
-
-  addPerson(person: Person) {
-    this.people.push(person);
-  }
-
-  getPeople() {
-    return this.people;
-  }
-}
